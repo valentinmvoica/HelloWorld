72e15c06809db24fbe661908dc7887f7b4376aa6
Valentin Voica <valentin_voiclla@live.com>
2022-04-28 19:59:13 +0300

    JDKASJKLADSLKJADSLKJ

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,8 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World2!");            
+            Console.WriteLine("Hello World2!");
+            Console.WriteLine("Hello World3!");
         }
     }
 }
