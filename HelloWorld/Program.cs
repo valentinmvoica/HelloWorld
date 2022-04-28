5d18eca61228573a509211dbaf55c8cfd5fe52cd
Valentin Voica <valentin_voiclla@live.com>
2022-04-28 19:56:38 +0300

    Deleted the last line

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,8 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World2!");
-            Console.WriteLine("Hello World3!");
+            Console.WriteLine("Hello World2!");            
         }
     }
 }
